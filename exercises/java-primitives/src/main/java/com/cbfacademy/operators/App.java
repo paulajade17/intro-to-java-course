6dee292ee8ede0fb156d934ecf5adf09f4467087
Paula Semakula-Buuza <paulasemakula-buuza@paulas-mbp.home>
2023-11-12 16:22:59 +0000

    Complete Java Primitives Exercises.

--- exercises/java-primitives/src/main/java/com/cbfacademy/operators/App.java
+++ exercises/java-primitives/src/main/java/com/cbfacademy/operators/App.java
@@ -6,26 +6,38 @@ public class App {
     }
 
     public static double add(double operand1, double operand2) {
-        throw new RuntimeException("Not implemented");
+        //throw new RuntimeException("Not implemented");
+
+       double add = operand1 + operand2;
     }
 
     public static double subtract(double operand1, double operand2) {
-        throw new RuntimeException("Not implemented");
+        //throw new RuntimeException("Not implemented");
+
+        double subtract = operand1 - operand2;
     }
 
     public static double multiply(double operand1, double operand2) {
-        throw new RuntimeException("Not implemented");
+        //throw new RuntimeException("Not implemented");
+
+        double multiply = operand1 * operand2;
+
     }
 
     public static Boolean areEqual(double operand1, double operand2) {
-        throw new RuntimeException("Not implemented");
+        //throw new RuntimeException("Not implemented");
+
+        Boolean areEqual = operand1 == operand2;
     }
 
     public static Boolean isLessThan(double operand1, double operand2) {
-        throw new RuntimeException("Not implemented");
-    }
+        //throw new RuntimeException("Not implemented");
+        
+        Boolean isLessThan = operand1 < operand2;
 
     public static Boolean isMoreThan(double operand1, double operand2) {
-        throw new RuntimeException("Not implemented");
+        //throw new RuntimeException("Not implemented");
+
+        Boolean isMoreThan = operand1 > operand2;
     }
 }
