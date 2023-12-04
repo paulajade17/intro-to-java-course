4a77b1bd82e00637eca6c894449e0b567c6354e3
Paula Semakula-Buuza <paulasemakula-buuza@paulas-mbp.home>
2023-12-04 22:27:14 +0000

    complete OOP exercise shapes part 2

---
+++ exercises/object-oriented-programming/src/main/java/com/cbfacademy/shapes/Paint.java
@@ -0,0 +1,15 @@
+package com.cbfacademy.shapes;
+//create a paint class
+public class Paint {
+    double coverage; //number of square feet per gallon
+
+    Paint(double coverage){
+        this.coverage = coverage;
+
+    }
+    public double getAmount(Shape shape){
+
+    return this.coverage;
+
+}
+}
