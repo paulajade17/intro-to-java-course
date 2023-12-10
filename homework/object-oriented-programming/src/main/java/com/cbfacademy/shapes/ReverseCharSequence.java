59313ff0a52c98de6b5ee33ce1fe77a8911b0e25
Paula Semakula-Buuza <paulasemakula-buuza@paulas-mbp.home>
2023-12-10 22:16:47 +0000

    complete OOP Homewored exercise 1

---
+++ homework/object-oriented-programming/src/main/java/com/cbfacademy/shapes/ReverseCharSequence.java
@@ -0,0 +1,31 @@
+package com.cbfacademy.shapes;
+import java.lang.CharSequence;
+
+
+public class ReverseCharSequence implements CharSequence{
+    //created a field called word
+    String word;
+    //created a constructor argument for String word
+    public ReverseCharSequence(String word){
+        this.word = word;
+        
+    }
+    // Use word.charAt to get a character of a string
+    public char charAt(int index){
+        return word.charAt(index);
+        
+        
+    }
+    public int length(){
+        return word.length();
+    }
+
+    public CharSequence subSequence(int start, int end){
+        return "ula";
+    }
+
+}
+
+// subSequence(0, 2) IS EQUAL TO pau
+// subSequence(2, 3) IS EQUAL TO ul
+// subSequence(1, 4) IS EQUAL TO aula
