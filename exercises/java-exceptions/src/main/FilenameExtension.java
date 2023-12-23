25c46e616b6cdb5ab63ea6d16d91b2d7207297c9
Paula Semakula-Buuza <paulasemakula-buuza@paulas-mbp.home>
2023-12-23 19:19:22 +0000

    complete exceptions exercise

--- exercises/java-exceptions/src/main/FilenameExtension.java
+++ exercises/java-exceptions/src/main/FilenameExtension.java
@@ -7,8 +7,31 @@ public class FilenameExtension extends Exception{
         if (filename == null || filename.isEmpty()) {
             throw new FilenameException("Filename cannot be null or empty");
         } 
+
+        else if(filename.endsWith(".java")){
+            return true;
+        }
+        else return false;
 }
-    public Map<String, int> map(List<String> filenames){
-        
+    public Map<String, Interger> map(List<String> filenames){
+
+        Map<String, Integer> myMap = new HashMap<>();
+        for (String file : filenames) {
+            try {
+                if(check(file) == true) {
+                    myMap.put(file, 1);
+                }
+                else {
+                    myMap.put(file, 0);
+                }
+
+            }
+            catch(FilenameException e){
+                myMap.put(file, -1);
+                e.printStackTrace();
+            }
+        }
+        return myMap;
+    
     }
 }
